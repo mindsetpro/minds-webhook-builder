65bdd035b8a0eaf05a363ac5b71a3b1ad1257b30
mindsetpro_ <138173273+mindsetpro@users.noreply.github.com>
2023-11-27 09:13:14 -0500

    Update and rename app.js to script.js

--- script.js
+++ script.js
@@ -5,7 +5,35 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));// Config
+const form = document.getElementById('form');
+const preview = document.getElementById('preview');
+
+// Color picker
+const picker = new iro // Init color picker
+
+// On submit
+document.getElementById('submit').addEventListener('click', () => {
+
+  // Get data
+  const data = new FormData(form);
+
+  // Send webhook
+  axios.post(data.get('webhookUrl'), {
+    content: data.get('message'),
+    embeds: [{
+      color: parseInt(data.get('color').replace('#', '0x')) 
+    }]
+  })
+  .then(() => {
+    console.log('Sent!');
+    preview.innerHTML = ''; // Clear preview
+  })
+  .catch(err => {
+    console.error('Error!', err);
+  })
+
+});
 app.use(express.static('public'));
 
 app.post('/send-webhook', async (req, res) => {
